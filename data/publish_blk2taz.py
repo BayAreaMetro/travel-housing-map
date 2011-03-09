062a5bd5e3fb4fa82c88d52651a0761069fd996b
allens <allens@26d378a2-6a99-40ce-9862-508a7a6a5e16>
2011-03-09 01:41:07 +0000

    Changed naming scheme of the blk2taz directories to be portable.
    git-svn-id: svn+ssh://studio.local/var/svn/mtcscs/trunk@33 26d378a2-6a99-40ce-9862-508a7a6a5e16

--- data/publish_blk2taz.py
+++ data/publish_blk2taz.py
@@ -5,16 +5,18 @@ def process_file(in_file, out_dir='.'):
 	fp = open(in_file, 'r')
 	reader = csv.DictReader(fp, dialect=mtc.MTCDialect)
 	written = 0
-	mtc.prepdirs(out_dir)
 	for row in iter(reader):
 		block_id = row["BLOCK"][1:]
 		taz_id = row["TAZ"]
-		filename = os.path.join(out_dir, '%s.txt' % block_id)
-		print >> sys.stderr, "%s -> %s" % (block_id, taz_id)
-		dp = open(filename, 'w')
-		dp.write(taz_id)
-		dp.close()
-		written += 1
+		parts = map(lambda i: block_id[i:i+3], range(0, len(block_id), 3))
+		if len(parts) > 0:
+			filename = os.path.join(out_dir, os.path.join(*parts[:-1]), "%s.txt" % parts[-1])
+			print >> sys.stderr, "%s / %s -> %s" % (block_id, taz_id, filename)
+			mtc.prepdirs(os.path.dirname(filename))
+			dp = open(filename, 'w')
+			dp.write(taz_id)
+			dp.close()
+			written += 1
 	fp.close()
 	return written
 
