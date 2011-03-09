83f14b99b867ef1c741a57dbf3f9a120408047f8
allens <allens@26d378a2-6a99-40ce-9862-508a7a6a5e16>
2011-03-09 04:14:58 +0000

    new blk2taz stuff
    git-svn-id: svn+ssh://studio.local/var/svn/mtcscs/trunk@36 26d378a2-6a99-40ce-9862-508a7a6a5e16

--- data/html/blk2taz/convert.js
+++ data/html/blk2taz/convert.js
@@ -1,10 +1,9 @@
-function blk2url(blk) {
-	var url = [],
-			step = 3;
-	for (var i = 0; i < blk.length; i += step) {
-		url.push(blk.substr(i, i + step));
+function blk2url(fips) {
+	var url = [], step = 3;
+	for (var i = 0; i < fips.length; i += step) {
+		url.push(fips.substr(i, step));
 	}
-	return "http://move.onebayarea.org/data/blk2taz/" + url.join("/");
+	return "http://move.onebayarea.org/data/blk2taz/" + url.join("/") + ".txt";
 }
 
 function blk2taz(block, options) {
@@ -15,8 +14,34 @@ function blk2taz(block, options) {
 	return $.ajax(url, opts);
 }
 
-function address2taz(addr, callback) {
-}
+function location2taz(loc, options) {
+	if (typeof loc == "string") {
+		loc = loc.split(/\s*,\s*/);
+		loc = {lat: loc[0], lon: loc[1]};
+	}
 
-function location2taz(loc, callback) {
+	var url = "http://data.fcc.gov/api/block/find";
+	return $.ajax(url, {
+		dataType: "jsonp",
+		data: {
+			latitude: loc.lat,
+			longitude: loc.lon,
+			format: "jsonp"
+		},
+		success: function(res) {
+			if (res.status == "OK") {
+				var fips = res.Block.FIPS;
+				// console.log("Got FIPS: " + fips);
+				if (options.fips) {
+					options.fips.call(options.context, fips);
+				}
+				blk2taz(fips, options);
+			} else {
+				if (res.Err) {
+					options.error.call(options.context, null, res.Err.msg, res.Err);
+				}
+			}
+		},
+		error: options.error
+	});
 }
