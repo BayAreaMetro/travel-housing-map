585d42930ab8b2eaced6a70f0bcd4869c4e89896
allens <allens@26d378a2-6a99-40ce-9862-508a7a6a5e16>
2011-03-09 00:58:58 +0000

    more moving
    git-svn-id: svn+ssh://studio.local/var/svn/mtcscs/trunk@30 26d378a2-6a99-40ce-9862-508a7a6a5e16

--- data/publish_scenario.sh
+++ data/publish_scenario.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python publish_scenario.py input/scenarios/$1/*.csv output/scenarios/$1
-python publish_blk2taz.py BLK00_MTCTAZ.csv output/block2taz
+python publish_blk2taz.py input/BLK00_MTCTAZ.csv output/block2taz
