5732d39c521ad307c44389540d6726e88c963053
Brandon Liu <bdon@bdon.org>
2014-02-10 11:20:41 -0800

    Add housing unit count properties to output taz1454.json.

--- data/merge_taz.py
+++ data/merge_taz.py
@@ -5,11 +5,10 @@ from optparse import OptionParser
 parser = OptionParser()
 opts, args = parser.parse_args()
 
-input_csv, input_json = map(lambda f: open(f, 'r'), args)
+home_price_csv, home_count_csv, input_json = map(lambda f: open(f, 'r'), args)
 
-rows = csv.DictReader(input_csv, dialect='excel')
-# CSV row foreign key
-input_key = "zone_id"
+price_rows = csv.DictReader(home_price_csv, dialect='excel')
+count_rows = csv.DictReader(home_count_csv, dialect='excel')
 
 null_value = -999
 
@@ -19,44 +18,49 @@ def coerce_int(s):
     return null_value
 
 output = json.load(input_json)
+
+# CSV row foreign key
+price_zone_key = "zone_id"
+count_zone_key = "TAZ"
 # GeoJSON feature foreign key
 output_key = "TAZ1454"
-# keys to merge from CSV to JSON features, and their transform value
-output_merge = {
-    "price10":      [coerce_int, "price_2010"],
-    "price20pr":    [coerce_int, "price_2020"],
-    "price30pr":    [coerce_int, "price_2030"],
-    "price40pr":    [coerce_int, "price_2040"],
+
+# the output key, the input key from housing price CSV,
+# the input key from housing count CSV
+price_mapping = {
+  "price_2010": "price10",
+  "price_2020": "price20pr",
+  "price_2030": "price30pr",
+  "price_2040": "price40pr"
 }
 
+count_mapping = {
+  "units_2010": "units10",
+  "units_2020": "units20",
+  "units_2030": "units30",
+  "units_2040": "units40"
+}
+
+# create a dictionary of all TAZ zones
 zones = {}
 for zone in output["features"]:
     zone_id = zone["properties"].get(output_key)
     zone["id"] = zone_id
     zones[str(zone_id)] = zone
 
-# rows = rows[::]
-# print >> sys.stderr, "%d rows" % len(rows)
+for price_row, count_row in zip(price_rows, count_rows):
+    zone_id = price_row.get(price_zone_key)
+    assert count_row.get(count_zone_key) == zone_id
 
-for row in rows:
-    zone_id = row.get(input_key)
     zone = zones.get(zone_id)
     if not zone:
         print >> sys.stderr, "NO SUCH ZONE:", zone_id
-        # sys.exit(1)
-        continue
-    for key, transform in output_merge.items():
-        if row.has_key(key):
-            value = row.get(key)
-            if transform:
-                if type(transform) in (list, tuple):
-                    transform, key = transform
-                value = transform(value)
-            print >> sys.stderr, "%s [%s] = %s" % (zone_id, key, value)
-            zone["properties"][key] = value
-        else:
-            print >> sys.stderr, "NO SUCH KEY:", key
-            # sys.exit(1)
+        sys.exit(1)
+
+    for o_key, i_key in price_mapping.items():
+      zone["properties"][o_key] = coerce_int(price_row.get(i_key))
+    for o_key, i_key in count_mapping.items():
+      zone["properties"][o_key] = coerce_int(count_row.get(i_key))
 
 json.encoder.FLOAT_REPR = lambda f: ("%.4f" % f)
 json.dump(output, sys.stdout, indent=None, separators=(",", ":"))
